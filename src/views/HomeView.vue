6349670ed452ba60813177b73a2026ff3c2e8198
HadesSoleil0462 <90576992+HadesSoleil0462@users.noreply.github.com>
2024-05-13 13:00:15 +0200

    Implement auto reopen RevisionView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
+  import router from '@/router'
+
+  let revision = localStorage.getItem('revision');
+  if (revision) {
+    let parsedRevision = JSON.parse(revision);
+    if (parsedRevision.started && new Date().getTime() - parsedRevision.startTime < 900000) {
+      router.push('/revision');
+    }
+  }
+
+  function startRevision() {
+    let revision: {started: boolean; startTime: number} = {
+        started: true,
+        startTime: new Date().getTime()
+    }
+    localStorage.setItem('revision', JSON.stringify(revision));
+
+    router.push('/revision')
+  }
 </script>
 
 <template>
   <main>
+    <h1> Welcome to HapplyMemory :) </h1>
+    <button @click="startRevision">Start revision</button>
   </main>
 </template>
