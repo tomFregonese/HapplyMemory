4a0c74797895dc82771acf1466ca1d391353ab74
HadesSoleil0462 <90576992+HadesSoleil0462@users.noreply.github.com>
2024-05-17 10:15:51 +0200

    [UPDATE] CategoriesView.vue -> trying to call CategoryItem.vue component

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,16 +1,35 @@
 <script setup lang="ts">
 import { onMounted, defineEmits } from 'vue'
 import router from '@/router'
+import CategoryItem from '@/components/CategoryItem.vue'
+import type { Category } from '@/models/Category'
+import type { Data } from '@/models/Data'
 
 const emit = defineEmits(['title'])
 
 onMounted(() => {
   emit('title', 'Categories')
 })
+
+let categories : Category[] = []
+if (localStorage.getItem('data')) {
+  let data : Data = JSON.parse(localStorage.getItem('data')!)
+  console.log(data)
+  for (let category of data.categories) {
+    categories.push(category)
+  }
+  console.log(categories)
+
+}
+
+
 </script>
 
 <template>
   <main>
+    <CategoryItem v-for="category in categories" :key="category.id" :title="category.title"
+                  :description="category.description" />
+
     <button @click="router.push('/create-category')">New category</button>
   </main>
 </template>
