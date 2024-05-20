d4e057019e1ae9788931fc97cb2f25d7ce118277
HadesSoleil0462 <90576992+HadesSoleil0462@users.noreply.github.com>
2024-05-20 14:17:18 +0200

    [UPDATE] App theme [CREATE] Create a Category page

--- src/views/CreateCategoryView.vue
+++ src/views/CreateCategoryView.vue
@@ -1,16 +1,46 @@
 <script setup lang="ts">
-import { onMounted, defineEmits } from 'vue'
+import { onMounted, defineEmits, ref } from 'vue'
+import type { Data } from '@/models/Data'
+import type { Category } from '@/models/Category'
 
 const emit = defineEmits(['title'])
 
 onMounted(() => {
   emit('title', 'Create a category')
 })
+
+let title = ref('')
+let description = ref('')
+
+const createCategory = () => {
+  let dataFromStorage = localStorage.getItem('data')
+  let data : Data;
+  if (dataFromStorage) {
+    data = JSON.parse(dataFromStorage).data
+  } else {
+    data = { categories: [] }
+  }
+  let newCategory : Category = {
+    id: (data.categories.length + 1),
+    title: title.value,
+    description: description.value,
+    themes: []
+  }
+  data.categories.push(newCategory)
+  localStorage.setItem('data', JSON.stringify({data: data}))
+}
+
 </script>
 
 <template>
   <main>
+    <form @submit.prevent="createCategory">
+      <input id="title" placeholder="Title" v-model="title" type="text" required>
 
+      <textarea id="description" placeholder="Description" v-model="description"></textarea>
+
+      <button type="submit">Create Category</button>
+    </form>
   </main>
 </template>
 
