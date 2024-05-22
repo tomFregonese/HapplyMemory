515d6ecab1c15881c93f3d6b2a5b185fad69d30c
HadesSoleil0462 <90576992+HadesSoleil0462@users.noreply.github.com>
2024-05-22 15:03:04 +0200

    [UPDATE] Add the ability to delete a category

--- src/components/CategoryItem.vue
+++ src/components/CategoryItem.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 import router from '@/router'
+import type { Data } from '@/models/Data'
 
 const props = defineProps<{
   title: string
@@ -12,12 +13,23 @@ function openTheme() {
   router.push({ name: 'Themes', params: { categoryId: props.id } })
 }
 
+function deleteCategory() {
+  let dataFromStorage = localStorage.getItem('data')
+  if (dataFromStorage) {
+    let data: Data = JSON.parse(dataFromStorage).data
+    data.categories = data.categories.filter(category => category.id !== props.id)
+    localStorage.setItem('data', JSON.stringify({ data }))
+    router.go(0)
+  }
+}
+
 </script>
 
 <template>
   <div @click="openTheme()">
     <h2>{{ props.title }}</h2>
     <p>{{ props.description }}</p>
+    <img @click.stop="deleteCategory()" src="../assets/trash-can-regular.svg" alt="Delete">
   </div>
 </template>
 
@@ -28,6 +40,11 @@ div {
   padding: 1em 2em;
   border-radius: 5px;
   background-color: white;
+}
+
+img {
+  right: 1em;
+  height: 1em;
 }
 
 @media (prefers-color-scheme: dark) {
