a567e9b530800cf69cdf2a8e627dd48d7237cada
HadesSoleil0462 <90576992+HadesSoleil0462@users.noreply.github.com>
2024-06-17 11:23:06 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HapplyMemoryIonicMigration',
+  webDir: 'dist'
+};
+
+export default config;
